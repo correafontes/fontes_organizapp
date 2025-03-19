cf3eac2931066574808cd910366b4a0e48b34788
CorreaFontes <davi.c.fontes@unesp.br>
2025-03-19 19:08:28 -0300

    add sqflite and path imports to createpage.dart

--- lib/createpage.dart
+++ lib/createpage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 class CreatePage extends StatefulWidget {
   const CreatePage({super.key});
