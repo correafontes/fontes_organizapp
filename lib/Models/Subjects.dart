841935bee1b6c0343e8229a46793f97e9b7a4adf
CorreaFontes <davi.c.fontes@unesp.br>
2025-03-19 19:04:13 -0300

    subjects class created

---
+++ lib/Models/Subjects.dart
@@ -0,0 +1,7 @@
+class Subject {
+  String name;
+  int hours;
+  String code;
+
+  Subject({required this.name, required this.hours, required this.code});
+}
--- lib/createpage.dart
+++ lib/createpage.dart
@@ -10,6 +10,8 @@ class CreatePage extends StatefulWidget {
 class _CreatePageState extends State<CreatePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
