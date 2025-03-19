5f1df78d8392d2f171fb4b7ec5cfea0fb48189ec
CorreaFontes <davi.c.fontes@unesp.br>
2025-03-19 18:45:07 -0300

    new app idea

---
+++ android/app/src/main/kotlin/com/example/fontes_organizapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fontes_organizapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/initializepage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Initializepage extends StatefulWidget {
+  const Initializepage({super.key});
+
+  @override
+  State<Initializepage> createState() => _InitializepageState();
+}
+
+class _InitializepageState extends State<Initializepage> {
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    return Scaffold(
+      body: Center(child: Column(children: [
+        SizedBox(height: mediaQuery.size.height * 0.15),
+        const Text(
+          'Bem Vindo(a)!',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: mediaQuery.size.height * 0.1),
+        ElevatedButton(onPressed: () {
+          
+        }, child: Text('Criar Arquivo'),),
+        SizedBox(height: mediaQuery.size.height * 0.075),
+        Divider(),
+        SizedBox(height: mediaQuery.size.height * 0.075),
+        ElevatedButton(onPressed: () {
+          
+        }, child: Text('Abrir Existente'),),
+      ],),),
+      bottomNavigationBar: BottomAppBar(
+        height: mediaQuery.size.height * 0.095,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Desenvolvido por: '),
+            TextButton(
+              onPressed: () {
+                //Abrir site
+              },
+              child: Text('Davi Fontes')
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
