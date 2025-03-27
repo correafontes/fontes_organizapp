5d51e204215f551a2cbd4ecb916c324576f3c5ab
CorreaFontes <davi.c.fontes@unesp.br>
2025-03-27 10:30:57 -0300

    Change from sqflite to sqlite3

--- lib/createpage.dart
+++ lib/createpage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:sqlite3/sqlite3.dart';
 import 'package:path/path.dart';
 
 class CreatePage extends StatefulWidget {
