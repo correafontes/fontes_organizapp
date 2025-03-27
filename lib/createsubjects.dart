c22c172c368a3cb607bfaba73b918ab1cd7d3cd1
Davi Correa Fontes <davi.c.fontes@unesp.br>
2025-03-27 11:33:01 -0300

    CreateSubjects page created

---
+++ lib/createsubjects.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Createsubjects extends StatefulWidget {
+  const Createsubjects({super.key});
+
+  @override
+  State<Createsubjects> createState() => _CreatesubjectsState();
+}
+
+class _CreatesubjectsState extends State<Createsubjects> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
